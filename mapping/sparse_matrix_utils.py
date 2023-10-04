d5e165ffb09faa0c109c88539fd8bd0d1909c0b8
SVivdich02 <vvv-sonya@mail.ru>
2023-10-05 01:18:29 +0300

    done visualization based on coo matrix for multiple views first version

--- mapping/sparse_matrix_utils.py
+++ mapping/sparse_matrix_utils.py
@@ -11,10 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import copy
 
 import numpy as np
 
 from scipy.sparse import coo_array
+
+from utils.image_utils import generate_random_colors
+from utils.pcd_utils import remove_hidden_points
+from utils.pcd_utils import get_point_map
+
+import open3d as o3d
 
 
 def construct_coo_matrix_for_multiple_views(coo_matrix_view_list, start_index):
@@ -50,3 +57,31 @@ def calculate_col(coo_matrix_view_list):
         col_list.append(np.array([i for i in range(len(view.data))], dtype=int))
 
     return np.concatenate([col for col in col_list])
+
+
+def visualize_labeled_clouds(coo_matrix_view_list, view_num, start_index, end_index, simpleMapping):
+    coo_matrix_view_list = coo_matrix_view_list.tocsr()
+    current_image_index = view_num
+    cam_name = 'cam2'
+
+    pcd_combined = get_point_map(cam_name, simpleMapping.dataset, start_index, end_index)
+    pcds_prepared = simpleMapping.dataset.prepare_points_before_mapping(
+        cam_name,
+        copy.deepcopy(pcd_combined),
+        start_index,
+        current_image_index
+    )
+    pcd_hidden_removal = remove_hidden_points(pcds_prepared)
+
+    coo_matrix = coo_matrix_view_list[[view_num - start_index],:]
+
+    random_colors = generate_random_colors(500)
+
+    colors = []
+    for i in range(coo_matrix.nnz):
+        colors.append(random_colors[int((coo_matrix_view_list[[view_num - start_index], [i]])[0]) + 1])
+
+    pcd_hidden_removal.colors = o3d.utility.Vector3dVector(np.vstack(colors) / 255)
+
+
+    return pcd_hidden_removal
