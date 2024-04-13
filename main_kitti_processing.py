acce1c8d803d8bce6255fe8ee80e80d1aef2aa67
SVivdich02 <vvv-sonya@mail.ru>
2024-04-13 11:38:28 +0300

    add scripts for processing the kitty dataset and for calculating result metrics from a csv file

---
+++ main_kitti_processing.py
@@ -0,0 +1,185 @@
+# Copyright (c) 2023, Sofia Vivdich and Anastasiia Kornilova
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+import numpy as np
+import pickle
+
+from scipy.spatial.distance import cdist
+
+from src.datasets.kitti_dataset import KittiDataset
+
+from src.services.distance.isolated import RemovingIsolatedPointsProcessor
+from src.services.distance.connected_component import ExtractionLargestConnectedComponentProcessor
+from src.services.normalized_cut_service import normalized_cut
+from src.services.preprocessing.common.config import ConfigDTO
+from src.services.preprocessing.init.map import InitMapProcessor
+from src.services.preprocessing.init.instances_matrix import (
+    InitInstancesMatrixProcessor,
+)
+from src.services.preprocessing.not_zero import SelectionNotZeroProcessor
+from src.services.preprocessing.in_cube import SelectionInCubeProcessor
+from src.services.preprocessing.statistical_outlier import StatisticalOutlierProcessor
+from src.services.preprocessing.voxel_down import VoxelDownProcessor
+
+from src.utils.distances_utils import sam_label_distance
+from src.utils.gt_utils import build_sem_inst_label_arrays
+
+
+dataset_path = "dataset/"
+sequence = "00"
+image_instances_path = "pipeline/vfm-labelss/sam/00/"
+gt_labels_path = "dataset/sequences/00/labels/"
+kitti = KittiDataset(dataset_path, sequence, image_instances_path)
+
+
+def build_tuple_bin_saving(config, pcd_for_clustering, trace, clusters, inst_label_array_for_clustering, sem_label_array_for_clustering):
+    params = {}
+    params['alpha_physical_distance'] = config.alpha_physical_distance
+    params['beta_instance_distance'] = config.beta_instance_distance
+    params['T_normalized_cut'] = config.T_normalized_cut
+    params['reduce_detail_int_to_union_threshold'] = config.reduce_detail_int_to_union_threshold
+    params['reduce_detail_int_to_mask_threshold'] = config.reduce_detail_int_to_mask_threshold
+
+    trace_arrays = []
+    for int_vector in (trace):
+        trace_arrays.append(np.asarray(int_vector))
+
+    return (params, np.asarray(pcd_for_clustering.points), trace_arrays, clusters, inst_label_array_for_clustering, sem_label_array_for_clustering)
+
+
+def segment_pcds(config):
+    init_pcd = InitMapProcessor().process(config)
+    points2instances = InitInstancesMatrixProcessor().process(config, init_pcd)
+
+    sem_label_array_src, inst_label_array_src = build_sem_inst_label_arrays(gt_labels_path,
+                                                                            config.start_index,
+                                                                            config.end_index)
+
+    processors = [
+        SelectionNotZeroProcessor(),
+        SelectionInCubeProcessor(),
+        StatisticalOutlierProcessor(),
+    ]
+
+    pcd = copy.deepcopy(init_pcd)
+    for processor in processors:
+        pcd, points2instances, indices = processor.process(config, pcd, points2instances)
+        inst_label_array_src = inst_label_array_src[indices]
+        sem_label_array_src = sem_label_array_src[indices]
+
+    pcd_for_clustering = copy.deepcopy(pcd)
+    points2instances_pcd_for_clustering = copy.deepcopy(points2instances)
+    inst_label_array_for_clustering = copy.deepcopy(inst_label_array_src)
+    sem_label_array_for_clustering = copy.deepcopy(sem_label_array_src)
+
+    pcd, points2instances, trace = VoxelDownProcessor().process(config, pcd, points2instances)
+
+    points = np.asarray(pcd.points)
+    spatial_distance = cdist(points, points)
+
+    dist, masks = sam_label_distance(points2instances, spatial_distance, 3, config.beta_instance_distance, config.alpha_physical_distance)
+
+    distance_processors = [
+        RemovingIsolatedPointsProcessor(),
+        ExtractionLargestConnectedComponentProcessor(),
+    ]
+
+    for processor in distance_processors:
+        dist, points, trace = processor.process(dist, points, trace)
+
+    eigenval = 2
+    clusters = normalized_cut(
+        dist,
+        np.array([i for i in range(len(points))], dtype=int),
+        config.T_normalized_cut,
+        eigenval
+    )
+
+    return build_tuple_bin_saving(
+        config,
+        copy.deepcopy(pcd_for_clustering),
+        copy.deepcopy(trace),
+        copy.deepcopy(clusters),
+        copy.deepcopy(inst_label_array_for_clustering),
+        copy.deepcopy(sem_label_array_for_clustering),
+    )
+
+
+def process_kitti(from_num, to_num, id_exec, alpha_physical_distance, beta_instance_distance):
+    T_normalized_cut = 0.03
+
+    reduce_detail_int_to_union_threshold = 0.5
+    reduce_detail_int_to_mask_threshold = 0.6
+
+    current_from_num = from_num
+
+    while (current_from_num < to_num):
+        start_index = current_from_num
+        end_index = start_index + 4
+        config = ConfigDTO(
+            **{
+                "dataset": kitti,
+                "start_index": start_index,
+                "end_index": end_index,
+                "start_image_index_offset": 0,
+                "alpha_physical_distance": alpha_physical_distance,
+                "beta_instance_distance": beta_instance_distance,
+                "T_normalized_cut": T_normalized_cut,
+                "reduce_detail_int_to_union_threshold": reduce_detail_int_to_union_threshold,
+                "reduce_detail_int_to_mask_threshold": reduce_detail_int_to_mask_threshold,
+                "cam_name": "cam2",
+                "R": 18,
+                "nb_neighbors": 25,
+                "std_ratio": 5.0,
+                "voxel_size": 0.25,
+            }
+        )
+
+        result_tuple = segment_pcds(config)
+
+        file_name = "experiment_bin_0704_4_sem_offset0_T0l03/start{}_end{}.pickle".format(config.start_index, config.end_index)
+        new_file = open(file_name, 'w')
+        new_file.close()
+
+        with open(file_name, 'wb') as file:
+            pickle.dump(result_tuple, file)
+
+        print("start_index={}, end_index={} done".format(start_index, end_index))
+        current_from_num = end_index
+
+
+def main():
+
+    # alpha_physical_distance_1 = 5
+    # beta_instance_distance_1 = 3
+    # process_kitti(0, 4, 1, alpha_physical_distance_1, beta_instance_distance_1)
+
+    # alpha_physical_distance_2 = 5
+    # beta_instance_distance_2 = 5
+    # process_kitti(0, 4, 2, alpha_physical_distance_2, beta_instance_distance_2)
+
+    # alpha_physical_distance_3 = 3
+    # beta_instance_distance_3 = 5
+    # process_kitti(0, 4, 3, alpha_physical_distance_3, beta_instance_distance_3)
+
+    alpha_physical_distance_4 = 5
+    beta_instance_distance_4 = 5
+    # T_4 = 0.025
+    # offset = 0
+    process_kitti(0, 4540, 4, alpha_physical_distance_4, beta_instance_distance_4)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_kitti_processing_metrics.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2023, Sofia Vivdich and Anastasiia Kornilova
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+import csv
+import numpy as np
+import pickle
+
+from evops.metrics import precision
+from evops.metrics import recall
+from evops.metrics import fScore
+
+
+def find_num_in_inst_label_array(src_points, inst_label_array_for_clustering):
+    for point in src_points:
+        if inst_label_array_for_clustering[point] > 0:
+            return inst_label_array_for_clustering[point]
+    return -1
+
+
+def build_pred_inst_array(inst_label_array_for_clustering, clusters, trace, instance_threshold):
+    pred_inst_array = np.zeros(len(inst_label_array_for_clustering), dtype=int)
+    k = 0
+    free_id = 1
+    for cluster in clusters:
+        voxel_not_in_gt_cluster_count = 0
+        for voxel in cluster:
+            src_points = trace[voxel]
+            id = find_num_in_inst_label_array(src_points, inst_label_array_for_clustering)
+            if id == -1:
+                voxel_not_in_gt_cluster_count += 1
+
+        cluster_in_gt_instance = ((len(cluster) - voxel_not_in_gt_cluster_count) / len(cluster)) * 100
+        if cluster_in_gt_instance >= instance_threshold:
+            for voxel in cluster:
+                src_points = trace[voxel]
+                for src_point in src_points:
+                    pred_inst_array[src_point] = free_id
+            free_id += 1
+        k += 1
+    return pred_inst_array
+
+
+def main():
+
+    from_num = 0
+    to_num = 4540
+
+    # execution_ids = [ 1, 2, 3, 4 ]
+    execution_ids = [ 4 ]
+    instance_thresholds = [ 5, 20, 30, 50 ]
+
+    for execution_id in execution_ids:
+
+        for instance_threshold in instance_thresholds:
+            print("Start to process instance_threshold={}".format(instance_threshold))
+
+            current_from_num = from_num
+
+            skipped = 0
+            while (current_from_num < to_num):
+                start_index = current_from_num
+                end_index = start_index + 4
+
+                file_name = "experiment_bin_0704_4_sem_offset0_T0l03/start{}_end{}.pickle".format(start_index, end_index)
+
+                with open(file_name, 'rb') as file:
+                    data = pickle.load(file)
+
+                trace = data[2]
+                clusters = data[3]
+                inst_label_array_for_clustering = data[4]
+
+                if inst_label_array_for_clustering.sum() == 0:  # в облаке нет инстансов => скип
+                    skipped += 1
+                    print("start_index={}, end_index={} skip".format(start_index, end_index))
+                    current_from_num = end_index
+                    continue
+
+                pred_inst_array = build_pred_inst_array(
+                    copy.deepcopy(inst_label_array_for_clustering),
+                    clusters,
+                    copy.deepcopy(trace),
+                    instance_threshold
+                )
+
+                pred_labels = pred_inst_array
+                gt_labels = inst_label_array_for_clustering
+                tp_condition = "iou"
+                precision_res = precision(pred_labels, gt_labels, tp_condition)
+                recall_res = recall(pred_labels, gt_labels, tp_condition)
+                fScore_res = fScore(pred_labels, gt_labels, tp_condition)
+
+                gt_labels_unique = set(gt_labels)
+                gt_labels_unique.discard(0)
+
+                pred_labels_unique = set(pred_labels)
+                pred_labels_unique.discard(0)
+
+                with open("experiment_1004_{}_without0_sem_offset0_T0l03_{}.csv".format(execution_id, instance_threshold), "a", newline="") as file:
+                    writer = csv.writer(file)
+
+                    writer.writerow([
+                        str(start_index),
+                        str(end_index),
+                        str(precision_res),
+                        str(recall_res),
+                        str(fScore_res),
+                        len(gt_labels_unique),
+                        len(pred_labels_unique),
+                        len(clusters),
+                    ])
+
+                current_from_num = end_index
+
+            print(skipped)
+            print("Finish to process instance_threshold={}".format(instance_threshold))
+
+
+if __name__ == "__main__":
+    main()
