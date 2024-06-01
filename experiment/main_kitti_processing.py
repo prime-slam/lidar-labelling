ea9193dc453f9b2d2c6aa78a1b0eb9e42dd71e7a
SVivdich02 <vvv-sonya@mail.ru>
2024-06-01 09:46:56 +0300

    refactoring

--- experiment/main_kitti_processing.py
+++ experiment/main_kitti_processing.py
@@ -17,7 +17,7 @@ import numpy as np
 import pickle
 import sys
 
-sys.path.append('/Users/sofiavivdich/proj/lidar-labelling')
+sys.path.append("/Users/sofiavivdich/proj/lidar-labelling")
 
 from scipy.spatial.distance import cdist
 
